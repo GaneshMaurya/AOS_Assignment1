de376a111eb046e9ebc1cf9fe31930e427ecdfd8
Ganesh <gvgmaurya@gmail.com>
2024-08-10 03:12:59 +0530

    reverse beginning and ending. garbage output

--- 2024201030_A1_Q1.cpp
+++ 2024201030_A1_Q1.cpp
@@ -71,7 +71,15 @@ int main(int argc, char *argv[])
             }
             else
             {
-                string str = inputDataBuffer;
+                string str = "";
+                for (int i = 0; i < BUFFER_SIZE; i++)
+                {
+                    if (inputDataBuffer[i] != ' ')
+                    {
+                        str += inputDataBuffer[i];
+                    }
+                }
+
                 double time1 = (double)clock() / CLOCKS_PER_SEC;
                 reverseString(str, 0, str.size() - 1);
 
@@ -107,8 +115,91 @@ int main(int argc, char *argv[])
     else if (flag == 1)
     {
         // Reverse first part and last part of file
-        int start_index = stoi(argv[4]);
-        int end_index = stoi(argv[5]);
+        int start_index = stoi(argv[3]);
+        int end_index = stoi(argv[4]);
+
+        if (start_index > end_index)
+        {
+            cout << "Error. Start index is greater than End index.\n";
+        }
+
+        if (mkdir(FOLDER_PATH, 0770) < 0 && errno == EEXIST)
+        {
+            if (rmdir(FOLDER_PATH) == 0)
+            {
+                cout << "Folder does not exist. Proceeding to create.\n";
+            }
+            else
+            {
+                cout << "Folder already exists. Hence deleting all its contents.\n";
+            }
+            mkdir(FOLDER_PATH, 0770);
+        }
+
+        // Open file in Read only mode
+        int inputFile = open(INPUT_FILE_PATH, O_RDWR);
+        if (inputFile < 0)
+        {
+            // Errors - Handle this
+            cout << "Error in opening the input file.\n";
+            return 0;
+        }
+        else
+        {
+            // Function to read a file. Reads n Bytes of the input.txt file and stores it in the below char array
+            char inputDataBuffer[BUFFER_SIZE];
+
+            // lseek(inputFile, BUFFER_SIZE, SEEK_SET);
+            ssize_t inputSize = read(inputFile, inputDataBuffer, BUFFER_SIZE);
+
+            if (inputSize == 0)
+            {
+                cout << "Error in reading the file.\n";
+                return 0;
+            }
+            else
+            {
+                string str = "";
+                for (int i = 0; i < BUFFER_SIZE; i++)
+                {
+                    if (inputDataBuffer[i] != ' ')
+                    {
+                        str += inputDataBuffer[i];
+                    }
+                }
+
+                double time1 = (double)clock() / CLOCKS_PER_SEC;
+                reverseString(str, 0, start_index - 1);
+                reverseString(str, end_index, str.size() - 1);
+
+                char outputDataBuffer[str.size()];
+                for (int i = 0; i < str.size(); i++)
+                {
+                    outputDataBuffer[i] = str[i];
+                }
+
+                string OUTPUT_FILE_NAME = "output.txt";
+                string OUTPUT_FILE_PATH = FOLDER_PATH + OUTPUT_FILE_NAME;
+
+                int outputFile = open(OUTPUT_FILE_PATH.c_str(), O_RDWR | O_CREAT | O_TRUNC, 0770);
+                if (outputFile < 0)
+                {
+                    // Errors - Handle this
+                    cout << " Error in opening the output file.\n";
+                    return 0;
+                }
+
+                ssize_t outputSize = write(outputFile, outputDataBuffer, str.size());
+                if (outputSize < 0)
+                {
+                    cout << "Error in writing content onto the file.\n";
+                    return 0;
+                }
+
+                double time2 = (double)clock() / CLOCKS_PER_SEC;
+                printf("File took %lfs to reverse and write onto new text file.\n", time2 - time1);
+            }
+        }
     }
     else
     {
